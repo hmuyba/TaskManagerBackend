394a606dae2fabe35c9ce85e8adadef8d0d03ec8
Harold Muyba <hmuyba@gmail.com>
2025-04-13 11:31:32 -0400

    fixing login redirect issue

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -65,7 +65,7 @@ const login = async (req, res) => {
     res.cookie("token", token, { 
       httpOnly: true, 
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'none' // Important for cross-site requests
+      sameSite: 'lax' // Important for cross-site requests
     }).json({ message: "Login successful", token });
   } catch (error) {
     console.error(error);
